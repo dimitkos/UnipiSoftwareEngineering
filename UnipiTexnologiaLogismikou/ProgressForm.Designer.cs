1512c18570ae7360271359dc4081d5a01c980534
Teo <mrsexobeatwr@gmail.com>
2019-04-16 12:36:27 +0300

    Esvisa to ID label kai to texbox ap to Progress

--- UnipiTexnologiaLogismikou/ProgressForm.Designer.cs
+++ UnipiTexnologiaLogismikou/ProgressForm.Designer.cs
@@ -29,10 +29,8 @@
         private void InitializeComponent()
         {
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.Viewbutton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.backbutton = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -40,24 +38,15 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(262, 167);
+            this.progressBar1.Location = new System.Drawing.Point(262, 157);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(100, 23);
             this.progressBar1.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(157, 109);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(18, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "ID";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(157, 167);
+            this.label2.Location = new System.Drawing.Point(157, 157);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 2;
@@ -72,13 +61,6 @@
             this.Viewbutton.Text = "View";
             this.Viewbutton.UseVisualStyleBackColor = true;
             this.Viewbutton.Click += new System.EventHandler(this.Viewbutton_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(262, 109);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
             // 
             // backbutton
             // 
@@ -105,10 +87,8 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.backbutton);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Viewbutton);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Name = "ProgressForm";
             this.Text = "ProgressForm";
@@ -121,10 +101,8 @@
         #endregion
 
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button Viewbutton;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button backbutton;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
